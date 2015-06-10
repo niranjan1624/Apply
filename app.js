6297d28a726b1f58fcbde770215172325ddb45d3
niranjan1624 <niranjan.ece1@gmail.com>
2015-06-10 10:11:50 +0530

    profile object created inside student object

--- app.js
+++ app.js
@@ -4,5 +4,43 @@ var student = {
 						
 						username: "",
 						password:""
+					},
+					
+					profile: {
+						personal_information: {
+							first_name:
+							middle_name:
+							last_name:
+							gender:
+							dob:
+							ssn_no:
+						},
+						address: {
+							country:
+							adress1:
+							adress2:
+							adress3:
+							city:
+							state:
+							zip_code:
+						},
+						contact_details: {
+							email:
+							phone_no:
+							skype_id:
+							
+						},
+						geography: {
+							country_of_birth:
+							city_of_birth:
+							citizenship:
+						},
+
+						disciplinary_history: {
+							disciplinary_history:
+						},
+						criminal_history: {
+							criminal_history:
+						}
 					}
 				  }
