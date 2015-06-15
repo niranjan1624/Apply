3b889bd8caac24c5fcc7c9c7b00486e29c6120f6
niranjan1624 <niranjan.ece1@gmail.com>
2015-06-15 15:07:02 +0530

    Registraion Info Page completed with validation of fields and colors of pages changed based on to FJU colors

--- app.js
+++ app.js
@@ -115,6 +115,7 @@ var student = {
 						}
 					}
 				  }
+var values = {};
 function createInput(ele,type,clsname,id,required){
 	var div = document.createElement("div");
 	var field = document.createElement("input");
@@ -126,9 +127,12 @@ function createInput(ele,type,clsname,id,required){
 	div.appendChild(field);
 	document.getElementById(ele).appendChild(div);
 }
-function createLabel(ele,name,value,id){
+function createLabel(ele,name,value,id,required){
 	var wrapper = $("#"+ele);
-	$(wrapper).append( '<br/><div> <label for='+name+' id='+id+'>'+value+'</label></div>');
+	if (required)
+		$(wrapper).append( '<br/><div class = "required"> <label for='+name+' id='+id+'>'+value+'</label></div>');
+	else
+		$(wrapper).append( '<div> <label for='+name+' id='+id+'>'+value+'</label></div>');
 }
 function createHiddenfield(ele,id) {
 	var field = document.createElement("p");
@@ -142,10 +146,10 @@ function showMsg(id,msg) {
 	$(id).html(msg);
 }
 $(document).ready(function() {
-
 	if(getCurentFileName() == "Apply.html")
 		loginCredential();
 	else if (getCurentFileName() == "Apply2.html") {
+		if(localStorage.user != undefined)  {
 		RegInfo();
 		$("#mob").intlTelInput();
 		 $("#mob").keydown(function (e) {
@@ -167,6 +171,9 @@ $(document).ready(function() {
 			}
 		}
     });
+	} else {
+		window.location.href = "Apply.html"
+	}
 	}
 });
 function insertBef(id) {
@@ -176,13 +183,17 @@ function RegInfo() {
 	
 	 var wrapper = $(".AccReg-form"); 
 	$(wrapper).append("<ul>")
-			 createLabel("Register1", "first_name", "First/given name","first");
+			 createLabel("Register1", "first_name", "First/given name","first",true);
 			 createHiddenfield("Register1","first_name_error");
 			 createInput("Register1", "text", "defaultTextInput input", "first_name",true);
 			 
-			 createLabel("Register1", "last_name", "Last/family/surname","last");
+			 createLabel("Register1", "last_name", "Last/family/surname","last",true);
 			 createHiddenfield("Register1","last_name_error");
 			 createInput("Register1", "text", "defaultTextInput input", "last_name",true);
+
+			  createLabel("register2", "skype", "Skype ID","last",false);
+			 createHiddenfield("register2","skype_error");
+			 createInput("register2", "text", "defaultTextInput input", "skype",false);
 
 	$(wrapper).append("</ul>");
 		 $("#first_name").focusout(function(){
@@ -194,6 +205,24 @@ function RegInfo() {
  $("#last_name").focusout(function(){
 		 validateField("#last_name", "text")
  });
+ $("#mob").focusout(function(){
+		 validateField("#mob", "mob")
+ });
+ $("#address1").focusout(function(){
+		 validateField("#address1", "text")
+ });
+  $("#address2").focusout(function(){
+		 validateField("#address2", "text")
+ });
+  $("#city").focusout(function(){
+		 validateField("#city", "text")
+ });
+  $("#state").focusout(function(){
+		 validateField("#state", "text")
+ });
+  $("#zipcode").focusout(function(){
+		 validateField("#zipcode", "text")
+ }); 
 
  
 }
@@ -235,25 +264,51 @@ function loginCredential(){
 		 validateField("#re_pass", "password")
  });
 }
-$('form#loginForm').submit(function() {
-	 console.log("submit");
-	if ($("#email").val() != $("#re_email").val())
-		showMsg("#re_email_error", "Email addresses must match.")
-	if ($("#pass").val() != $("#re_pass").val())
-		showMsg("#re_pass_error", "Passwords must match.")
-	else
-		console.log("success");
+
+$('form#Reg-Form').submit(function() {
+
+	$("form#Reg-Form :input").each(function(){
+		var input = $(this);
+		var value = input.val();
+		var id = input.attr("id");
+		values[id] = value;
+		
+		var selectedVal = "";
+	});
+	var selected = $("#radioDiv input[type='radio']:checked");
+	if (selected.length > 0) {
+		selectedVal = selected.val();
+		values["gender"] = selectedVal;
+	}
+	if (validateRegFields(values) && validateField("#mob","mob")) {
+		// send all the data to server (data store)
+		console.log(assignValuesJson(values).login_credentials);
+		console.log("passed")
+	} else {
+		//same page should be shown show the errors
+		console.log("notPassed")
+	}
+	
+	console.log(values);
  });
+
  $('form#loginForm').submit(function() {
 	 console.log("submit");
 	if ($("#email").val() != $("#re_email").val())
 		showMsg("#re_email_error", "Email addresses must match.")
 	if ($("#pass").val() != $("#re_pass").val())
 		showMsg("#re_pass_error", "Passwords must match.")
-	else
+	else {
 		console.log("success");
+		values["email"]  = $("#email").val();
+		values["pass"] = $("#pass").val();
+		assignValuesJson(values);
+		console.log(localStorage.user);
+		window.location.href = "Apply2.html"
+	}
  });
 function validateField(id,type) {
+	var pass = false;
 	var value = $(id).val();
 	id = id+"_error";
 	if(value=="")
@@ -261,15 +316,17 @@ function validateField(id,type) {
 	else {
 	switch (type) {
 		case "email":
-			console.log(value[value.length - 1]);
+			
 			if(value.indexOf("@") == -1)
 				showMsg(id, "Email must include one @.")
 			else if(value.indexOf("@.") != -1 || value.indexOf(".@") != -1 || value[value.length - 1] == ".")
 				showMsg(id,"Email before and after @ cannot start or end with a dot.")
 			else if(value[value.length - 1] == "@")
 				showMsg(id, "invalid email id.")
-			else
+			else {
 				$(id).hide();
+				pass = true;
+			}
 			break;
 		case "password":
 			var msg = ""
@@ -287,13 +344,79 @@ function validateField(id,type) {
 				msg = msg + "Password should not have space character(s).<br/>"
 			if(msg != "")
 				showMsg(id,msg);
-			else
-				$(id).hide();
+			else {
+				$(id).hide(); 
+				pass = true;
+			}
 			break;
-
-		
+		case "mob":
+			if(value.length < 7) 
+				showMsg(id,"Please complete the required field")
+			else if(value.substring(0,2) == "+1" && value.length != 12)
+				showMsg(id, "Phone - For country code +1 your phone number must be 10 digits.")
+			else if(value.length <= 9 || value.length >= 16)
+				showMsg(id,"Phone - Phone number must be from 7 to 14 digits.");
+			else {
+				$(id).hide();
+				pass = true;
+			}
+			break;
+		default:
+			$(id).hide();
 	}
 	}
+	return pass;
+}
+function validateRegFields(values){
+	var pass = true;
+	var zip = values['zipcode'];
+	var mm = values["month"]
+	var dd = values["day"]
+	var yy = values["year"]
+	if (mm==null || dd==null || yy==null)  {
+		showMsg("#dob_error", "Please complete the required field");
+		pass  = false
+		return false 
+	}
+	var ListofDays = [31,28,31,30,31,30,31,31,30,31,30,31];
+		if (mm==1 || mm>2)
+              {
+                  if (dd>ListofDays[mm-1])
+                  {
+                      //alert('Invalid date format!');
+                      pass = false;
+					  showMsg("#dob_error","Invalid Date of Birth");
+					  return false
+                  }
+              }
+              if (mm==2)
+              {
+                  var lyear = false;
+                  if ( (!(yy % 4) && yy % 100) || !(yy % 400))
+                  {
+                      lyear = true;
+                  }
+                  if ((lyear==false) && (dd>=29))
+                  {
+					  showMsg("#dob_error","Invalid Date of Birth");
+					  pass = false
+                      return false;
+                  }
+                  if ((lyear==true) && (dd>29))
+                  {
+					  showMsg("#dob_error","Invalid Date of Birth");
+                      pass = false
+                      return false;
+                  }
+              }  
+		if(zip.length > 10) {
+			showMsg("#zipcode_error", "Enter valid zip code");
+			pass  = false;
+			return false;
+		}
+		if(pass)
+			$("#dob_error").hide();
+	return pass
 }
 function disableCopyPaste(id){
 	  $(id).bind("cut copy paste",function(e) {
@@ -304,4 +427,31 @@ function getCurentFileName(){
     var pagePathName= window.location.pathname;
     return pagePathName.substring(pagePathName.lastIndexOf("/") + 1);
 }
-console.log(student.login_credentials.username)
+function assignValuesJson(value) {
+	if(getCurentFileName() == "Apply.html") {
+		localStorage.user = values['email']
+		localStorage.pass = values['pass'];
+	}
+	else if (getCurentFileName() == "Apply2.html") {
+		student.login_credentials.username = localStorage.user;
+		student.login_credentials.password = localStorage.pass;	
+		student.profile.contact_details.email = localStorage.user;
+		student.profile.personal_information.first_name = values['first_name'];
+		student.profile.personal_information.last_name = values['last_name'];
+		student.profile.personal_information.gender = values['gender'];
+		student.profile.personal_information.dob = values['month']+"/"+ values['day'] + "/" + values['year'];
+		student.profile.address.country = values['country'];
+		student.profile.address.address1 = values['address1'];
+		student.profile.address.address2 = values['address2'];
+		student.profile.address.address3 = values['address3'];
+		student.profile.address.city = values['city'];
+		student.profile.address.state = values['state'];
+		student.profile.address.zip_code = values['zipcode'];
+		student.profile.contact_details.phone_no = value['mob'];
+		student.profile.contact_details.skype_id = value['skype'];
+		localStorage.clear();
+	}
+
+		localStorage.ques190 = student;
+		return student;
+}
